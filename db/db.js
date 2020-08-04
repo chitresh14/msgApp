0b83d7be62c853d1ef2788bc66fab3fed76b5060
unknown <355chitresh@gmail.com>
2020-08-04 23:18:11 +0530

    MONGODB_URI in db

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-console.log("process.env.MONGODB_URL", process.env.MONGODB_URL)
-mongoose.connect(process.env.MONGODB_URL, {
+console.log("process.env.MONGODB_URI", process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     // useFindAndModify: true,
