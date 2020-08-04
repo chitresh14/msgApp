2358a0e95ef18f84e509a1e93a728eab1d4faa50
unknown <355chitresh@gmail.com>
2020-08-04 15:42:48 +0530

    Added optional port

--- app.js
+++ app.js
@@ -22,9 +22,7 @@ app.use(userRouter);
 
 
 //Listeniong to Port
-const server = app.listen(5000 , () => {
-    console.log('app is running on port:', PORT);
-});
+const server = app.listen(PORT || 5000);
 
 // server timeout
 server.timeout = 50000;
