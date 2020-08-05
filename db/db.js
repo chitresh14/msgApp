2c1978736aa19d2a2901858389172eaf00468fdd
unknown <355chitresh@gmail.com>
2020-08-05 09:03:07 +0530

    Console log in db

--- db/db.js
+++ db/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+console.log("process.env.MONGODB_URI:::::::0",process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
