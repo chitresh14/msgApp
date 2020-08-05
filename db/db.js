696b8d9f5384954a83a2eb8207a40bdcd895de1a
unknown <355chitresh@gmail.com>
2020-08-05 08:14:05 +0530

    added MONGOLAB_GOLD_URI

--- db/db.js
+++ db/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-console.log("process.env.MONGODB_URI", process.env.MONGODB_URI)
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGOLAB_GOLD_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     // useFindAndModify: true,
