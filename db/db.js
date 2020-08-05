aa030fc688bdcaa2d747e06210a3478d0258b949
unknown <355chitresh@gmail.com>
2020-08-05 09:21:38 +0530

    db.js changes

--- db/db.js
+++ db/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 console.log("process.env.MONGODB_URI:::::::0",process.env.MONGODB_URI);
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    // useFindAndModify: true,
-    useUnifiedTopology: true
-});
+
+mongoose.connect(process.env.MONGODB_URI)
+// mongoose.connect(process.env.MONGODB_URI, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     // useFindAndModify: true,
+//     useUnifiedTopology: true
+// });
