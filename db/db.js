3d988da594dafbb847b616d7b45e792ffdef5e5e
unknown <355chitresh@gmail.com>
2020-08-05 09:44:27 +0530

    Added mongoose connection

--- db/db.js
+++ db/db.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose');
 
 console.log("process.env.MONGODB_URI:::::::0",process.env.MONGODB_URI);
 
-mongoose.connect(process.env.MONGODB_URI)
-// mongoose.connect(process.env.MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     // useFindAndModify: true,
-//     useUnifiedTopology: true
-// });
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    // useFindAndModify: true,
+    useUnifiedTopology: true
+});
